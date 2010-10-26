489723d8f5a4ca58fbeabd18bca76a31c321d718
Gerhard Lazu <gerhard@lazu.co.uk>
2010-10-26 14:24:56 +0100

    This was restarting Apache too often It's more verbose, but it's explicit

--- recipes/rutorrent_apache.rb
+++ recipes/rutorrent_apache.rb
@@ -1,10 +1,19 @@
-bash "enable Apache modules" do
-  code <<-EOH
-  a2enmod scgi
-  a2enmod auth_digest
-  a2enmod ssl
-  EOH
-  notifies :reload, resources(:service => "apache2"), :immediately
+bash "Enable Apache SCGI" do
+  code "a2enmod scgi"
+  notifies :reload, resources(:service => "apache2")
+  not_if { File.symlink?("/etc/apache2/mods-enabled/scgi.load") }
+end
+
+bash "Enable Apache AuthDigest" do
+  code "a2enmod auth_digest"
+  notifies :reload, resources(:service => "apache2")
+  not_if { File.symlink?("/etc/apache2/mods-enabled/auth_digest.load") }
+end
+
+bash "Enable Apache SSL" do
+  code "a2enmod ssl"
+  notifies :reload, resources(:service => "apache2")
+  not_if { File.symlink?("/etc/apache2/mods-enabled/ssl.load") }
 end
 
 template "/etc/apache2/sites-available/rutorrent-ssl" do
@@ -13,14 +22,25 @@ template "/etc/apache2/sites-available/rutorrent-ssl" do
   backup false
 end
 
-bash "enable Apache rutorrent-ssl" do
+bash "Disable Apache default site" do
   cwd "/etc/apache2"
-  code <<-EOH
-  a2dissite default
-  a2dissite default-ssl
-  a2ensite rutorrent-ssl
-  EOH
-  notifies :reload, resources(:service => "apache2"), :immediately
+  code "a2dissite default"
+  notifies :reload, resources(:service => "apache2")
+  not_if { !File.exists?("/etc/apache2/sites-enabled/000-default") }
+end
+
+bash "Disable Apache default SSL site" do
+  cwd "/etc/apache2"
+  code "a2dissite default-ssl"
+  notifies :reload, resources(:service => "apache2")
+  not_if { !File.exists?("/etc/apache2/sites-enabled/default-ssl") }
+end
+
+bash "Enable Apache rutorrent SSL site" do
+  cwd "/etc/apache2"
+  code "a2ensite rutorrent-ssl"
+  notifies :reload, resources(:service => "apache2")
+  not_if { File.exists?("/etc/apache2/sites-enabled/rutorrent-ssl") }
 end
 
 execute "chown -fR www-data:www-data /var/www/rutorrent"
